e59327130d4f8efed935fb82f38b62162571a6b9
Christopher Hong <chrishong294@gmail.com>
2024-04-08 21:57:34 -0400

    update deps

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 
 class PytorchConan(ConanFile):
     name = "pytorch-conan"
-    requires = "abseil/20240116.1@chong/dev", "protobuf/26.0@chong/dev", "onnx/1.15.0@chong/dev", "openssl/3.2.0@chong/dev"
+    requires = "abseil/20240116.1@chong/dev", "protobuf/26.1@chong/dev", "onnx/1.16.0@chong/dev", "openssl/3.2.1@chong/dev"
     python_requires = "shell/0.0.1"
 
     def layout(self):
